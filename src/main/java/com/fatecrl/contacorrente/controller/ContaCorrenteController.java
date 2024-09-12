c03660b9b93cca89e8ef3d84f5b84bf8b85c641f
Bruno Santos <bruno_silva1420@hotmail.com>
2024-09-12 19:58:11 -0300

    Adicionado o application.yml e realizada a configuração do Controller e ContaService.java

--- src/main/java/com/fatecrl/contacorrente/controller/ContaCorrenteController.java
+++ src/main/java/com/fatecrl/contacorrente/controller/ContaCorrenteController.java
@@ -1,11 +1,26 @@
 package com.fatecrl.contacorrente.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fatecrl.contacorrente.model.Conta;
+import com.fatecrl.contacorrente.service.ContaService;
+
 @RestController
 @RequestMapping("/conta-corrente")
 public class ContaCorrenteController {
+
+    @Autowired
+    private ContaService contaService;
+
+    @GetMapping
+    public List<Conta> getAll(){
+        return contaService.findAll();
+    }
+
 
 }
