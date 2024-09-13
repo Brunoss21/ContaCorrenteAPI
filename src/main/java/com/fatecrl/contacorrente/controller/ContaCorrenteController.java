d10dcb7837e5b07637d547f0e0adbb47ed265d9b
Bruno Santos <bruno_silva1420@hotmail.com>
2024-09-12 21:27:44 -0300

    Adicionados os metodos POST, PUT e DELETE.

--- src/main/java/com/fatecrl/contacorrente/controller/ContaCorrenteController.java
+++ src/main/java/com/fatecrl/contacorrente/controller/ContaCorrenteController.java
@@ -1,11 +1,20 @@
 package com.fatecrl.contacorrente.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.fatecrl.contacorrente.model.Conta;
 import com.fatecrl.contacorrente.service.ContaService;
@@ -18,9 +27,49 @@ public class ContaCorrenteController {
     private ContaService contaService;
 
     @GetMapping
-    public List<Conta> getAll(){
-        return contaService.findAll();
+    public ResponseEntity<List<Conta>> getAll(){
+        return ResponseEntity.ok(contaService.findAll());
     }
 
+    //Para executar: htpp://localhost:8090/api/conta-corrente/1 (ok)
+    //Para executar: htpp://localhost:8090/api/conta-corrente/99 (notfound)
+    @GetMapping("/{id}")
+    public ResponseEntity<Conta> get(@PathVariable("id") Long id){
+        Conta conta = contaService.find(id);
+
+        if(conta != null){
+            return ResponseEntity.ok(conta);
+        }
+
+       return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Conta> create(@RequestBody Conta conta){
+        contaService.create(conta);
+        URI location = ServletUriComponentsBuilder
+                            .fromCurrentRequest()
+                            .path("/{id}")
+                            .buildAndExpand(conta.getId())
+                            .toUri();
+        return ResponseEntity.created(location).body(conta);
+
+    }
+
+    @PutMapping
+    public ResponseEntity<Conta> update(@RequestBody Conta conta){
+        if(contaService.update(conta)){
+            return ResponseEntity.ok(conta);
+       }
+       return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Conta> delete(@PathVariable("id") long id){
+        if(contaService.delete(id)){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 
 }
